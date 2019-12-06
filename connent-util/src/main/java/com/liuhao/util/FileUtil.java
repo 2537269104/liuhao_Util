6ffd6fd44f4362686eeceaaea7c823b4a02f329c
额emme“ <2537269104@qq.com>
2019-12-06 19:21:55 +0800

    完善文件工具类以及流工具类

--- connent-util/src/main/java/com/liuhao/util/FileUtil.java
+++ connent-util/src/main/java/com/liuhao/util/FileUtil.java
@@ -1,5 +1,103 @@
 package com.liuhao.util;
 
-public class FileUtil {
+import java.io.File;
+import java.text.DecimalFormat;
 
+public class FileUtil {
+       /**
+        * 
+        * @Title: getExtName 
+        * @Description: 获取文件名的拓展名 
+        * @param @param fileName
+        * @param @return    设定文件 
+        * @return String    返回类型 
+        * @throws
+        */
+	public static String getExtName(String fileName) {
+		
+		String extName = fileName.substring(fileName.lastIndexOf("."));
+		
+		return extName;
+	}
+	/**
+	 * 
+	 * @Title: deleteFile 
+	 * @Description: 递归删除文件
+	 * @param @param file    设定文件 
+	 * @return void    返回类型 
+	 * @throws
+	 */
+	public static void deleteFile(File file) {
+		//isDirectory() 判断该文件是否是文件夹
+		if(file.isDirectory()) {
+			File[] listFiles = file.listFiles();
+			for (File file2 : listFiles) {
+				deleteFile(file2);
+			}   
+			file.delete();
+		}
+		else {
+			file.delete();
+		}
+		
+		
+	}
+	/**
+	 * 
+	 * @Title: deleteFile 
+	 * @Description: 删除文件
+	 * @param @param filePath    设定文件 
+	 * @return void    返回类型 
+	 * @throws
+	 */
+	public static void deleteFile(String filePath) {
+		
+		deleteFile(new File(filePath));
+	} 
+	
+	
+	/**
+	 * 
+	 * @Title: getSystemUserHome 
+	 * @Description: 获取操作系统用户目录 
+	 * @param @param args    设定文件 
+	 * @return String    返回类型 
+	 * @throws
+	 */
+	public static String getSystemUserHome() {
+		
+		return System.getProperty("user.home");
+	}
+	
+	
+	/**
+	 * 
+	 * @Title: getFileSizeOfB 
+	 * @Description: 文件的指定大小 KB结尾
+	 * @param @param file
+	 * @param @return    设定文件 
+	 * @return String    返回类型 
+	 * @throws
+	 */
+	public static String  getFileSize(File file) {
+		
+		long size = file.length();
+		
+		float i = (float) (size/1024.0f);
+		/*
+		 * String s = String.format("%.2f", i);
+		 * System.out.println("============"+s);
+		 */
+		DecimalFormat df = new DecimalFormat("#.00");
+		return df.format(i)+"kb";
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		//deleteFile("C:\\Users\\25372\\Desktop\\test");
+	     System.out.println(getSystemUserHome());
+	
+	    System.out.println(getFileSize(new File("C:\\Users\\25372\\Desktop\\大数据（CMS）-第6单元（技能）.doc")));
+	}
 }
---
+++ connent-util/src/main/java/com/liuhao/util/StreamUtil.java
@@ -0,0 +1,189 @@
+package com.liuhao.util;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamUtil {
+
+	/**
+	 * 
+	 * @Title: closeStream 
+	 * @Description: 关流
+	 * @param @param autoCloseables    设定文件 
+	 * @return void    返回类型 
+	 * @throws
+	 */
+	public static void closeStream(AutoCloseable...autoCloseables) {
+		
+		for (AutoCloseable autoCloseable : autoCloseables) {
+			if(autoCloseable!=null) {
+				try {
+					autoCloseable.close();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	
+	/**
+	 * 
+	 * @Title: readTextFile 
+	 * @Description: 读取文件中的内容
+	 * @param @param file
+	 * @param @return    设定文件 
+	 * @return String    返回类型 
+	 * @throws
+	 */
+	@SuppressWarnings("resource")
+	public static String  readTextFile(File file) {
+		     InputStream input =null;
+		     String str="";
+		try {
+			 input = new  FileInputStream(file);
+			byte[] b = new byte[1024];
+			while( input.read(b)!=-1) {
+				
+				str+=new String(b);
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			closeStream(input);
+		}
+		
+		return str;
+		
+	}	
+	
+	/**
+	 * 
+	 * @Title: readTextFileByLine 
+	 * @Description: 按行读取文件内容
+	 * @param @param file
+	 * @param @return    设定文件 
+	 * @return List<String>    返回类型 
+	 * @throws
+	 */
+	
+	
+	public static List<String> readTextFileByLine(File file) {
+		
+		BufferedReader br =null;
+		  List<String> list = new ArrayList<>();
+		  try {
+			   
+			br = new BufferedReader(new FileReader(file));
+			  String str="";
+			  //此处你用fin.read方法已经读取一个字符了二循环体又用readline方法
+			  //接着读显然会少一个字符因为你read方法读过一个字符游标就会移动一下
+			while((str=br.readLine())!=null) {
+				list.add(str);
+				list.add("\r\n");
+			}
+			
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+            closeStream(br);
+		}
+		
+		return list;
+	}
+	/**
+	 * 
+	 * @Title: copyStream 
+	 * @Description: TODO复制流
+	 * @param @param input
+	 * @param @param out
+	 * @param @param isCloseInputStream
+	 * @param @param isCloseOutputStream    设定文件 
+	 * @return void    返回类型 
+	 * @throws
+	 */
+	
+	public static void copyStream(InputStream input,OutputStream out,boolean isCloseInputStream,boolean isCloseOutputStream) {
+		
+		
+		
+	}
+	
+	/**
+	 * 
+	 * @Title: writeFileContext 
+	 * @Description: 
+	 * @param @param path
+	 * @param @param file    设定文件 
+	 * @return void    返回类型 
+	 * @throws
+	 */
+	
+      @SuppressWarnings("resource")
+	   public static void  writeFileContext(String context,File file,boolean append) {
+    	  
+    	  BufferedWriter bw = null;
+    	  
+    	  try {
+			
+			 //判断所写的文件是否有父级路径
+			 String parent = file.getParent();
+			 File parentFile = new File(parent);
+			 if(!parentFile.exists()) {
+				 parentFile.mkdirs();
+			 }
+			 //写文件
+			 bw = new BufferedWriter(new FileWriter(file,append));
+			 bw.write(context);
+			 bw.flush();
+			 
+			 
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			closeStream(bw);
+		}
+      }
+	
+	 public static void downFileFromNet() {
+		 
+	 }
+	
+	public static void main(String[] args) {
+		
+		//String readTextFile = readTextFile(new File("C:\\Users\\25372\\Desktop\\hello.txt"));
+		//System.out.println(readTextFile);
+	
+//	     List<String> list = readTextFileByLine(new File("C:\\Users\\25372\\Desktop\\hello.txt"));
+//	     
+//	     for (String str : list) {
+//			System.out.println(str);
+//		}
+		String readTextFile = readTextFile(new File("C:\\Users\\25372\\Desktop\\hello.txt"));
+		System.out.println(readTextFile);
+	      writeFileContext("aaaaaaaaaa", new File("C:\\Users\\25372\\Desktop\\hello2.txt"), true);
+	}
+}
